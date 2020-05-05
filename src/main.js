96fe98804dd3a9d240b25486089c16973ef11c39
17978ithpower <2809023278@qq.com>
2020-05-05 11:39:10 +0800

    第二天,按照国际惯例,先提交代码,后打卡,晚上加鸡腿,老板请洗脚

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import VueRouter from 'vue-router'
 // 安装路由
 Vue.use(VueRouter)
 
+// 导入格式化时间的插件
+import moment from 'moment'
+// 定义全局的过滤器
+Vue.filter('dataFormat', function(dataStr, pattern = "YYYY-MM-DD HH:mm:ss"){
+  return moment(dataStr).format(pattern)
+})
+
 // 导入 mui 的样式
 import './lib/mui/css/mui.css'
 import './lib/mui/css/icons-extra.css'
@@ -14,6 +21,8 @@ import './lib/mui/css/icons-extra.css'
 import VueResource from 'vue-resource'
 // 安装 vue-resource
 Vue.use(VueResource)
+// 设置请求的根路径
+Vue.http.options.root = 'http://www.liulongbin.top:3005'
 
 // 导入 mint-ui js 组件
 import { Swipe, SwipeItem } from 'mint-ui';
